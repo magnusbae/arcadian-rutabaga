2faa4e1113b27c2b8975158b80126a4ab8a57aa5
jan banan <jan.arne.sandnes@itera.no>
2015-11-21 13:57:55 +0000

    Første POST OK

--- photobooth.py
+++ photobooth.py
@@ -1,10 +1,32 @@
 import picamera
 import time
+import datetime
 import sys
+import json
+import urllib2
+import base64
+import requests
+
+def postImage(timestamp, filename):
+    print ("posting image...")
+    with open(filename, "rb") as image_file:
+        encodedData = base64.b64encode(image_file.read())
+    data = {
+        'timestamp': timestamp,
+        'filename': filename,
+        'image': encodedData
+    }
+    #req = urllib2.Request('http://itera-photobooth.herokuapp.com/api/images')
+    #req.add_header('Content-Type', 'application/json')
+    #response = urllib2.urlopen(req, json.dumps(data))
+    #print("POST response: " + response)
+    url = 'http://itera-photobooth.herokuapp.com/api/images'
+    #payload = {'some': 'data'}
+    headers = {'content-type': 'application/json'}
+    response = requests.post(url, data=json.dumps(data), headers=headers)
+    
 
 camera = picamera.PiCamera()
-
-bildenr = 1
 
 try:
     print("start preview")
@@ -12,7 +34,12 @@ try:
     #time.sleep(5)
     print("Enter for  ta bilde...")
     input = sys.stdin.readline()
-    camera.capture("testfil" + str(bildenr) + ".jpg")
+    #ts = time.time()
+    #filename = "photo-" + datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%dT%H:%M:%S') + ".jpg"
+    ts = datetime.datetime.utcnow().isoformat()
+    filename = "photo-" + ts + ".jpg"
+    camera.capture(filename)
+    postImage(ts, filename)
     camera.stop_preview()
     print("stop preview")
 finally:
--- test.py
+++ test.py
@@ -1,5 +1,17 @@
 import sys
+import time
+import datetime
 
 print "Testing:"
 input = sys.stdin.readline()
 print "You wrote: " + input
+
+ts = time.time()
+print("Time: " + str(ts))
+
+
+time2 = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%dT%H:%M:%S')
+
+print("Time2: " + time2)
+
+
